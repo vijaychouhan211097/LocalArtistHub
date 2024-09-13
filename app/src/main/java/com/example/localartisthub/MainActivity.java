4c297cbb7a193abc3f03e833ae685cce18966be3
vijaychouhan211097 <vijaychouhan211097@gmail.com>
2024-09-13 19:20:21 +0530

    complete sign/signup page with firebase

--- app/src/main/java/com/example/localartisthub/MainActivity.java
+++ app/src/main/java/com/example/localartisthub/MainActivity.java
@@ -1,13 +1,8 @@
 package com.example.localartisthub;
 
 import android.content.Intent;
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
